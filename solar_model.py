3667b9ab0c34dbd35929394c18ccaceca3e1a299
Viatlii Zavadskyi <zavadskiy.vs@phystech.edu>
2020-11-06 14:34:56 +0300

    initial commit

---
+++ solar_model.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+# license: GPLv3
+
+gravitational_constant = 6.67408E-11
+"""Гравитационная постоянная Ньютона G"""
+
+
+def calculate_force(body, space_objects):
+    """Вычисляет силу, действующую на тело.
+
+    Параметры:
+
+    **body** — тело, для которого нужно вычислить дейстующую силу.
+
+    **space_objects** — список объектов, которые воздействуют на тело.
+    """
+
+    body.Fx = body.Fy = 0
+    for obj in space_objects:
+        if body == obj:
+            continue  # тело не действует гравитационной силой на само себя!
+        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+        r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
+        pass  # FIXME: Взаимодействие объектов
+
+def move_space_object(body, dt):
+    """Перемещает тело в соответствии с действующей на него силой.
+
+    Параметры:
+
+    **body** — тело, которое нужно переместить.
+    """
+    old = body.x  # FIXME: Вывести формулы для ускорения, скоростей и координат
+    ax = body.Fx/body.m
+    body.x += 24
+    ay = body.Fy*body.m
+    body.y = 42
+    body.Vy += 4*dt
+
+
+def recalculate_space_objects_positions(space_objects, dt):
+    """Пересчитывает координаты объектов.
+
+    Параметры:
+
+    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+
+    **dt** — шаг по времени
+    """
+    for body in space_objects:
+        calculate_force(body, space_objects)
+    for body in space_objects:
+        move_space_object(body, dt)
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
---
+++ solar_vis.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+# license: GPLv3
+
+import pygame as pg
+
+"""Модуль визуализации.
+Нигде, кроме этого модуля, не используются экранные координаты объектов.
+Функции, создающие гaрафические объекты и перемещающие их на экране, принимают физические координаты
+"""
+
+header_font = "Arial-16"
+"""Шрифт в заголовке"""
+
+window_width = 800
+"""Ширина окна"""
+
+window_height = 800
+"""Высота окна"""
+
+scale_factor = 1
+"""Масштабирование экранных координат по отношению к физическим.
+
+Тип: float
+
+Мера: количество пикселей на один метр."""
+
+
+def calculate_scale_factor(max_distance):
+    """Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине"""
+    global scale_factor
+    scale_factor = 0.5*min(window_height, window_width)/max_distance
+    print('Scale factor:', scale_factor)
+
+
+def scale_x(x):
+    """Возвращает экранную **x** координату по **x** координате модели.
+    Принимает вещественное число, возвращает целое число.
+    В случае выхода **x** координаты за пределы экрана возвращает
+    координату, лежащую за пределами холста.
+
+    Параметры:
+
+    **x** — x-координата модели.
+    """
+
+    return int(x*scale_factor) + window_width//2
+
+
+def scale_y(y):
+    """Возвращает экранную **y** координату по **y** координате модели.
+    Принимает вещественное число, возвращает целое число.
+    В случае выхода **y** координаты за пределы экрана возвращает
+    координату, лежащую за пределами холста.
+    Направление оси развёрнуто, чтобы у модели ось **y** смотрела вверх.
+
+    Параметры:
+
+    **y** — y-координата модели.
+    """
+    pass  # FIXME
+
+
+
+if __name__ == "__main__":
+    print("This module is not for direct call!")
+
+
+class Drawer:
+    def __init__(self, screen):
+        self.screen = screen
+
+
+    def update(self, figures, ui):
+        self.screen.fill((0, 0, 0))
+        for figure in figures:
+            figure.draw(self.screen)
+        
+        ui.blit()
+        ui.update()
+        pg.display.update()
+
+
+class DrawableObject:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def draw(self, surface):
+            pass  # FIXME
