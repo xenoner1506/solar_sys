2595b074e3fe8a73c6cafc3c18dd9cb9cc884f5f
Maksim Chalyj <calyjmaksim40@mail.ru>
2022-11-24 22:50:53 +0300

    test3

--- solar_input.py
+++ solar_input.py
@@ -1,8 +1,4 @@
-# coding: utf-8
-# license: GPLv3
 
-from solar_objects import Star, Planet
-from solar_vis import DrawableObject
 
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
